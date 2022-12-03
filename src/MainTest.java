3fd234aa415d40a778ad4384a69c9517d5869aaf
Ulyana <u.lyzhina@indriver.com>
2022-12-03 23:42:52 +0300

    Second test

--- src/MainTest.java
+++ src/MainTest.java
@@ -6,4 +6,10 @@ public class MainTest {
     public void testGetLocalNumber() {
         Assert.assertTrue("Метод returnFourteen не возвращает 14", new MainClass().getLocalNumber() == 14);
     }
+
+    @Test
+    public void testGetClassNumber() {
+        Assert.assertTrue("Метод getClassNumber возвращает число < 45 или == 45",
+                new MainClass().getClassNumber() > 45);
+    }
 }
