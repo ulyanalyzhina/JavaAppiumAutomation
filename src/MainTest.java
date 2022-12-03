294ec821a10af42c8b1f35d9b2b238619f2fa28c
Ulyana <u.lyzhina@indriver.com>
2022-12-03 23:48:12 +0300

    Third test

--- src/MainTest.java
+++ src/MainTest.java
@@ -12,4 +12,11 @@ public class MainTest {
         Assert.assertTrue("Метод getClassNumber возвращает число < 45 или == 45",
                 new MainClass().getClassNumber() > 45);
     }
+
+    @Test
+    public void testGetClassString() {
+        Assert.assertTrue("getClassString не возвращает строку, в которой есть подстрока “hello” или “Hello”",
+                new MainClass().getClassString().contains("Hello")
+                        || new MainClass().getClassString().contains("hello"));
+    }
 }
